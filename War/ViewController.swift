8b0f820ad4f0a0dc0418f9820b7951d341b57c0e
Spark <sparkboldstudio@gmail.com>
2017-11-01 10:02:05 -0500

    add @IBOutlet for cards and scores

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rightImageView: UIImageView! //! make var with value in it
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var cpuScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
